f5622b1f61c0d2388e7a18ddee23cbecc8b28198
Iulia <iulialucicadutu@gmail.com>
2022-10-29 21:25:04 +0300

    am adaugat rutarea si functionalitatea unor butoane

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Pages from "./pages/Pages";
 import React from 'react';
 import Category from "./components/Category";
+import {BrowserRouter} from 'react-router-dom' ;
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
-      <Category />
-      <Pages />
+      <BrowserRouter>
+       <Search />
+       <Category />
+       <Pages />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState, React } from "react";
 import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css/sea-green";
+import {Link} from 'react-router-dom';
 
 function Veggie() {
   const [veggie, setVeggie] = useState([]);
@@ -47,9 +48,11 @@ function Veggie() {
                 return (
                   <SplideSlide key={recipe.id}>
                     <Card>
+                      <Link to={"/recipe/"+recipe.id}>
                       <p>{recipe.title}</p>
                       <img src={recipe.image} alt={recipe.title} />
                       <Gradient />
+                      </Link>
                     </Card>
                   </SplideSlide>
                 );
